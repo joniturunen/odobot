535739b1b92ce95b27f163c96c5e0c365f589956
Joni Turunen <joni.turunen@triplan.fi>
2022-09-27 22:27:47 +0300

    Change Bean to support list of Events

--- src/main/java/run/joni/odobot/configuration/OdoConf.java
+++ src/main/java/run/joni/odobot/configuration/OdoConf.java
@@ -2,12 +2,17 @@ package run.joni.odobot.configuration;
 
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import run.joni.odobot.eventlisteners.EventListener;
+
+import java.util.List;
+
 
 @Configuration
 @PropertySource("classpath:application.yml")
@@ -33,13 +38,23 @@ public class OdoConf {
 
 
     @Bean
-    public GatewayDiscordClient gatewayDiscordClient() {
+    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
         LOG.info("Creating Discord client...");
         LOG.info("OdoBot version: " + ODO_VERSION);
-        return DiscordClientBuilder.create(odoDiscordToken)
+        GatewayDiscordClient client = DiscordClientBuilder.create(odoDiscordToken)
                 .build()
                 .login()
                 .block();
+
+        for(EventListener<T> listener : eventListeners) {
+           client.on(listener.getEventType())
+                   .flatMap(listener::execute)
+                   .onErrorResume(listener::handleError)
+                   .subscribe();
+        }
+
+        return client;
+
     }
 
 }
